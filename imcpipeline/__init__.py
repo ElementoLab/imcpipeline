c586411f95a0fdffdfdd49232b7d8aadfa52ee72
afrendeiro <afrendeiro@gmail.com>
2020-05-19 20:18:28 -0400

    compartmentalize graphical functions to new module; add memoization capability
    Former-commit-id: e7e9469bb6f22a166da2f87e49f7448b0d4d9063
    Former-commit-id: c56c150be253902884b47ae26b68d28a94cba041

--- imcpipeline/__init__.py
+++ imcpipeline/__init__.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 
+import os
 import sys
 import logging
-import matplotlib.pyplot as plt
 
+from joblib import Memory
+import matplotlib.pyplot as plt
 
 plt.rcParams['svg.fonttype'] = 'none'
 
@@ -22,5 +24,8 @@ def setup_logger(level=logging.INFO):
 
 LOGGER = setup_logger()
 
+# Setup joblib memory
+JOBLIB_CACHE_DIR = os.path.join(os.path.expanduser("~"), ".imcpipeline")
+MEMORY = Memory(location=JOBLIB_CACHE_DIR, verbose=0)
 
 from imcpipeline.data_models import Project, IMCSample, ROI
